c1d59055566c1fd95228b3f7333b36034c7195f2
Florent <florent.andre2000@gmail.com>
2021-09-28 10:50:08 +0200

    Intégration du template

---
+++ resources/views/template/partials/_main.blade.php
@@ -0,0 +1,11 @@
+<section class="ftco-section ftco-degree-bg">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 ftco-animate">
+            @yield('content')
+          </div>
+
+        @include('template.partials._aside')
+         
+      </div>
+    </section> <!-- .section -->
